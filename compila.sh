02d4ea097472ab4d3dd4e59a469adf3d34a7cc36
Agustin Garcia-Reynoso <agustin@atmosfera.unam.mx>
2019-12-09 10:01:05 -0600

    change in compila to use F90 files

--- compila.sh
+++ compila.sh
@@ -13,7 +13,7 @@ echo $ProcessDir
 #
 #  
 cd  02_aemis/
-    ifort -o ASpatial.exe -O3 area_espacial.f90 &
+    ifort -o ASpatial.exe -O3 area_espacial.F90 &
     wait
     ./ASpatial.exe > ../ejecuta.log &
 cd ../03_movilspatial/
@@ -28,23 +28,24 @@ cd ../03_movilspatial/
 cd ../04_temis
    ifort -O3 -axAVX atemporal.f90 -o Atemporal.exe &
 cd ../05_semisM
-    ifort -O3 -axAVX -o MSpatial.exe movil_spatial.f90 
+    ifort -O3 -axAVX -o MSpatial.exe movil_spatial.F90 
     ./MSpatial.exe >> ../ejecuta.log &
 cd ../06_temisM
     ifort -O3 movil_temp.f90 -o Mtemporal.exe &
 cd ../07_puntual
-    ifort -O3 -axAVX  t_puntal.f90 -o Puntual.exe &
+    ifort -O3 -axAVX  t_puntal.F90 -o Puntual.exe &
 cd ../08_spec
     ifort -O2 -axAVX  agg_a.f90 -o spa.exe
-    ifort -O2 -axAVX agg_m.f90 -o spm.exe
+    ifort -O2 -axAVX  agg_m.f90 -o spm.exe
     ifort -O2 -axAVX  agg_p.f90 -o spp.exe &
 cd ../09_pm25spec
-ifort -O2 -axAVX  pm25_speci_a.f90 -o spm25a.exe &
-ifort -O2 -axAVX  pm25_speci_m.f90 -o spm25m.exe &
-ifort -O2 -axAVX  pm25_speci_p.f90 -o spm25p.exe &
+ifort -O2 -axAVX  pm25_speci_a.F90 -o spm25a.exe &
+ifort -O2 -axAVX  pm25_speci_m.F90 -o spm25m.exe &
+ifort -O2 -axAVX  pm25_speci_p.F90 -o spm25p.exe &
 cd ../10_storage/
 ifort -O2 -axAVX -lnetcdff -L$NETCDF/lib -I$NETCDF/include g_2014_racm.f90 -o racm2.exe &
 ifort -O2 -axAVX -lnetcdff -L$NETCDF/lib -I$NETCDF/include  g_cbm5_2014.f90 -o cbm5.exe &
 ifort -O2 -axAVX -lnetcdff -L$NETCDF/lib -I$NETCDF/include g_radm_2014.f90 -o radm2.exe &
 ifort -O2 -axAVX -lnetcdff -L$NETCDF/lib -I$NETCDF/include g_saprc_2014.f90 -o saprc.exe
 cd ..
+echo "Done Compila"
